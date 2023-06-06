229c72e424b8f78df9c95be7a3452e8c23f4a317
EgorPoprotskiy <EgorPoprotskiy@gmail.com>
2023-06-06 11:34:24 +1000

    Creating a layout

--- app/src/main/java/com/egorpoprotskiy/servicestest/MainActivity.kt
+++ app/src/main/java/com/egorpoprotskiy/servicestest/MainActivity.kt
@@ -2,10 +2,14 @@ package com.egorpoprotskiy.servicestest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.egorpoprotskiy.servicestest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
