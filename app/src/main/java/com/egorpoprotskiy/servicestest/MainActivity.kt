1eec02732db714a2b844fbb26f3a03f93792f6ce
EgorPoprotskiy <EgorPoprotskiy@gmail.com>
2023-06-07 13:12:36 +1000

    NotificationManager

--- app/src/main/java/com/egorpoprotskiy/servicestest/MainActivity.kt
+++ app/src/main/java/com/egorpoprotskiy/servicestest/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.egorpoprotskiy.servicestest
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
 import com.egorpoprotskiy.servicestest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +19,40 @@ class MainActivity : AppCompatActivity() {
         binding.simpleService.setOnClickListener {
             startService(MyService.newIntent(this, 25))
         }
+        binding.foregroundService.setOnClickListener {
+            showNotification()
+        }
+    }
+    //Уведомление пользователю
+    private fun showNotification() {
+        //получение экземпляра для получения уведомления.
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        //Создание канала для Андроид с API>=26
+        //проверка, если версия андроид>8
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                //Приоритет уведомления
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+                //Установка Заголовока
+            .setContentTitle("Title")
+                //Установка Текста
+            .setContentText("Text")
+                //Установка иконки
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .build()
+        //Отображение уведомления, через созданный экземпляр(Только для API>=26)(Добавить строку в манифест при версии Android>13)
+        //<uses-permission android:name="android.permission.POST_NOTIFICATIONS" />
+        //id - для показа количества уведомлений.
+        notificationManager.notify(1, notification)
+    }
+    companion object {
+        private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME = "channel_name"
     }
 }
