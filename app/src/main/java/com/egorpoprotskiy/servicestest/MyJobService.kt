5ba7c6227e72cc57efc15c3456744e494f1d54b6
EgorPoprotskiy <EgorPoprotskiy@gmail.com>
2023-06-08 16:13:57 +1000

    JobScheduler2

--- app/src/main/java/com/egorpoprotskiy/servicestest/MyJobService.kt
+++ app/src/main/java/com/egorpoprotskiy/servicestest/MyJobService.kt
@@ -1,11 +1,9 @@
 package com.egorpoprotskiy.servicestest
 
-import android.app.Service
 import android.app.job.JobParameters
 import android.app.job.JobService
-import android.content.Context
 import android.content.Intent
-import android.os.IBinder
+import android.os.Build
 import android.util.Log
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -14,7 +12,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 
-class MyJobService: JobService() {
+class MyJobService : JobService() {
 
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
@@ -25,18 +23,27 @@ class MyJobService: JobService() {
 
     override fun onStartJob(params: JobParameters?): Boolean {
         log("onStartCommand")
-        coroutineScope.launch {
-            for (i in 0 until 100) {
-                delay(1000)
-                log("Timer $i")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            coroutineScope.launch {
+                var workItem = params?.dequeueWork()
+                while (workItem != null) {
+                    val page = workItem.intent.getIntExtra(PAGE, 0)
+                        for (i in 0 until 5) {
+                            delay(1000)
+                            log("Timer $i $page")
+                        }
+                        params?.completeWork(workItem)
+                        workItem = params?.dequeueWork()
+                    }
+                    //вызывается, если требуется перезапуск сервиса, true - требуется, false - не требуется
+                    jobFinished(params, false)
+                }
             }
-            //вызывается, если требуется перезапуск сервиса, true - требуется, false - не требуется
-            jobFinished(params, true)
-        }
         //если сервис закончил работу и он больше не требуется, то true
         //если нам надо, чтобы он продолжил работу, то false
         return true
     }
+
     //вызывается только если система сама убила сервис
     override fun onStopJob(params: JobParameters?): Boolean {
         log("onStopJob")
@@ -54,6 +61,12 @@ class MyJobService: JobService() {
     }
 
     companion object {
-        const val JOB_ID = 0
+        const val JOB_ID = 111
+        private const val PAGE = "page"
+        fun newIntent(page: Int): Intent {
+            return Intent().apply {
+                putExtra(PAGE, page)
+            }
+        }
     }
 }
